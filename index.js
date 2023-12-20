dd60eb39e2354a8cbadca68dd109b64b7c1666e0
abd <abdullahashraf00780078@gmail.com>
2023-12-20 22:19:14 +0500

    create a new projects files

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+//  var rollNumber1 : number=1
+//  var rollNumbe2 : number=2
+// var rollNumber3: number=3
+//  var rollNumber4 : number=4
+// var rollNumber5: number=5
+var studentsName = ["ali", "hamza", "salman", "bilal",];
+function pushNewItemInArray(abdullah) {
+    studentsName[3] = "studentName";
+}
+pushNewItemInArray("zeeshan");
+console.log(studentsName);
---
+++ index.ts
@@ -0,0 +1,81 @@
+//  var rollNumber1 : number=1
+//  var rollNumbe2 : number=2
+// var rollNumber3: number=3
+//  var rollNumber4 : number=4
+// var rollNumber5: number=5
+
+
+ var studentsName:string[]=["ali","hamza","salman","bilal",]
+
+
+function pushNewItemInArray (abd:string) {
+    
+studentsName[3]="studentName"
+
+
+
+}
+pushNewItemInArray("zeeshan");
+
+console.log(studentsName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
